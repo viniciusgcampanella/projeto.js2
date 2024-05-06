7e17222af649cac4491d59748edaf838191880d5
viniciusgcampanella <vgcgalvao5122@gmail.com>
2024-05-06 17:22:02 -0300

    projetojs2

---
+++ script.js
@@ -0,0 +1,32 @@
+let option 
+let items = []
+
+while(option !=3) {
+    option = Number(prompt(`
+    Olá usuario! Digite o número da opção deseja
+    
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados 
+    3. Sair do programa`  
+    ))
+
+   switch(option){
+    case 1:
+        let item = prompt ("Digite o nome do item")
+        items.push(item)
+        break
+        case 2:
+            if(items.length ==0){
+                alert("Não existem itens cadastrados")
+            }
+            else{
+                alert(items)
+            }
+            break
+            case 3:
+                alert("Goodbye")
+                break
+                default:
+                    alert("Opção Invalida. Tente novamente!")
+   }
+}
